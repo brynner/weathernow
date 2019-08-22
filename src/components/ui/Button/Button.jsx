3b4707e1ed8bc058606ae6749b9daeef2f20359c
Brynner Ferreira <ferreira@brynner.net>
2019-08-22 12:50:40 -0300

    Update Button.jsx
    Validação de propriedades, e adição de propriedade.

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Button.scss';
 
 export default class Button extends Component {
 
+    /**
+     * Validar tipos de props
+     */
+    static get propTypes() { 
+        return {
+            reloadCard: PropTypes.func,
+            text: PropTypes.string
+        }; 
+    }
+
     render() {
         return(
             <main className="component-button">
-                <button className="btn btn-default" onClick={() => this.props.reloadCard()}>Try Again</button>
+                <button className="btn btn-default" onClick={() => this.props.reloadCard()}>{this.props.text}</button>
             </main>
         )
     }
